9711eaf0f11be44307762f525bc94d908d10ba49
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-05-16 15:28:16 +0100

    Generate annotate for guidance and information clearance

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: recipes
+#
+#  id               :bigint           not null, primary key
+#  cooking_time     :integer
+#  description      :text
+#  name             :string
+#  preparation_time :integer
+#  public           :boolean
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 class Recipe < ApplicationRecord
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: recipes
+#
+#  id               :bigint           not null, primary key
+#  cooking_time     :integer
+#  description      :text
+#  name             :string
+#  preparation_time :integer
+#  public           :boolean
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do

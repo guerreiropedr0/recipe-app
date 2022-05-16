9a2a3d0ee8abf8f77f369625d434028a1a68e772
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-05-16 15:03:02 +0100

    Generate recipe model

---
+++ spec/factories/recipes.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :recipe do
+    name { "MyString" }
+    preparation_time { 1 }
+    cooking_time { 1 }
+    description { "MyText" }
+    public { false }
+  end
+end

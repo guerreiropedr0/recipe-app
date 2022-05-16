6fa66cc1abfc3fa7d4fa1d4a95f01c7f11d89c37
tYohannes <teklegy@gmail.com>
2022-05-16 17:39:59 +0300

    Add User model spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
   validates :name, length: { maximum: 100 }, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,41 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user, name: 'Tekle') }
+
+  describe 'validate data: ' do
+    it 'name should be present' do
+      user.name = ''
+      expect(user).to_not be_valid
+    end
+
+    it 'name should be present' do
+      user.name = 'John'
+      expect(user).to be_valid
+    end
+
+    it 'name length should not be greate than 100 characters' do
+      user.name = 'JohnLorem ipsum dolor sit amet, consectetuer adipiscing \
+        elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis \
+        natoque penatibus et magnis dis parturient montes, nascetur ridiculus \
+        mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, \
+        sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel,\
+        aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet \
+        a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. \
+        Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean \
+        vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat \
+        vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, \
+        feugiat a, tellus.'
+      expect(user).to_not be_valid
+    end
+  end
 end

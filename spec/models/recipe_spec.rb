6f94acd5845eaa91e485cb8d65fa70f5b2e9961b
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-05-16 16:32:29 +0100

    Change rspec before :example to FactoryBot gem
    Used the factorybot gem tool to simplify testing

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -15,78 +15,78 @@ require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
   context 'validation' do
-    before :example do
-      @recipe = Recipe.create(name: 'French macaroons', preparation_time: '20', cooking_time: '15',
-                              description: 'Delicious snack', public: true)
+    let(:recipe) do
+      FactoryBot.create(:recipe, name: 'French macaroons', preparation_time: '20', cooking_time: '15',
+                                 description: 'Delicious snack', public: true)
     end
 
     it 'should be valid with valid attributes' do
-      expect(@recipe).to be_valid
+      expect(recipe).to be_valid
     end
 
     it 'should be invalid without a name' do
-      @recipe.name = nil
+      recipe.name = nil
 
-      expect(@recipe).to_not be_valid
+      expect(recipe).to_not be_valid
     end
 
     it 'should be invalid with a description under 1 character and over 200 characters' do
-      @recipe.description = ''
+      recipe.description = ''
 
-      expect(@recipe).to_not be_valid
+      expect(recipe).to_not be_valid
 
-      @recipe.description = 'a' * 201
+      recipe.description = 'a' * 201
 
-      expect(@recipe).to_not be_valid
+      expect(recipe).to_not be_valid
     end
 
     it 'should be invalid with without a public value' do
-      @recipe.public = nil
+      recipe.public = nil
 
-      expect(@recipe).to_not be_valid
+      expect(recipe).to_not be_valid
     end
   end
 
   context 'preparation_time and cooking_time columns' do
-    before :example do
-      @recipe = Recipe.create(name: 'French macaroons', preparation_time: '20', cooking_time: '15',
-                              description: 'Delicious snack', public: true)
+    let(:recipe) do
+      FactoryBot.create(:recipe, name: 'French macaroons', preparation_time: '20', cooking_time: '15',
+                                 description: 'Delicious snack', public: true)
     end
 
     it 'should be invalid without a preparation time' do
-      @recipe.preparation_time = nil
+      recipe.preparation_time = nil
 
-      expect(@recipe).to_not be_valid
+      expect(recipe).to_not be_valid
     end
 
     it 'should be invalid with a preparation time under 1' do
-      @recipe.preparation_time = 0
+      recipe.preparation_time = 0
 
-      expect(@recipe).to_not be_valid
+      expect(recipe).to_not be_valid
     end
 
     it 'should be invalid with a preparation time over 2880' do
-      @recipe.preparation_time = 2881
+      recipe.preparation_time = 2881
 
-      expect(@recipe).to_not be_valid
+      expect(recipe).to_not be_valid
     end
 
     it 'should be invalid without a cooking time' do
-      @recipe.cooking_time = nil
+      recipe.cooking_time = nil
 
-      expect(@recipe).to_not be_valid
+      expect(recipe).to_not be_valid
     end
 
     it 'should be invalid with a cooking time under 1' do
-      @recipe.cooking_time = 0
+      recipe.cooking_time = 0
 
-      expect(@recipe).to_not be_valid
+      expect(recipe).to_not be_valid
     end
 
     it 'should be invalid with a cooking time over 2880' do
-      @recipe.cooking_time = 2881
+      recipe.cooking_time = 2881
 
-      expect(@recipe).to_not be_valid
+      expect(recipe).to_not be_valid
     end
   end
 end

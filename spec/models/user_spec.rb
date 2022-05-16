b515d4d2e740ecd1a0e48dc832f9efabf8f05291
tYohannes <teklegy@gmail.com>
2022-05-16 18:25:34 +0300

    Refactor long string name to use (puts 'a' * 5) format as per code review feedback

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,17 +24,7 @@ RSpec.describe User, type: :model do
     end
 
     it 'name length should not be greate than 100 characters' do
-      user.name = 'JohnLorem ipsum dolor sit amet, consectetuer adipiscing \
-        elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis \
-        natoque penatibus et magnis dis parturient montes, nascetur ridiculus \
-        mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, \
-        sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel,\
-        aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet \
-        a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. \
-        Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean \
-        vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat \
-        vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, \
-        feugiat a, tellus.'
+      user.name = puts 'John' * 26
       expect(user).to_not be_valid
     end
   end

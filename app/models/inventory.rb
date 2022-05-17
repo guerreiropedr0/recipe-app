f7cd22f44755dd33573880dc3fd971f08c48fd6a
oluyaratosin123 <oluyaratosin123@gmail.com>
2022-05-17 12:54:39 +0100

    Recipe app - resolve branch conflicts

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -2,9 +2,4 @@ class Inventory < ApplicationRecord
   validates :name, presence: true
 
   belongs_to :user, class_name: 'user', foreign_key: 'user_id'
-  has_many :recipes
-
-  def three_most_recent_inventories
-    inventories.order(created_at: :desc).limit(3)
-  end
 end

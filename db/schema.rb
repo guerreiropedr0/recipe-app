0943dab83383886d372be2df24ec33c898d76c6d
tYohannes <teklegy@gmail.com>
2022-05-18 18:14:30 +0300

    Add delete food functionality to foods list view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_18_005355) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_18_004447) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,7 +29,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_18_005355) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_inventories_on_user_id"
   end
 

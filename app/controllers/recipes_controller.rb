83eddaefb1c1b198d967f8d9ec61beee278a3b88
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-05-18 16:16:39 +0100

    Style and create new recipe registration form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,13 +11,35 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find_by(id: params[:id])
   end
 
-  def new; end
+  def new
+    if current_user
+      @recipe = Recipe.new
+    else
+      redirect_to root_path, alert: 'You need to login in order to create a recipe.'
+    end
+  end
 
-  def create; end
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+
+    if @recipe.save
+      redirect_to recipes_path, notice: 'Successfully created recipe.'
+    else
+      flash.now[:alert] = 'Could not create recipe.'
+      render :new
+    end
+  end
 
   def destroy
     redirect_to root_path, notice: 'Successfully deleted recipe.' if Recipe.destroy(params[:id])
   end
 
   def public_recipes; end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :cooking_time, :preparation_time, :public)
+  end
 end

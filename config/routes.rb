462537d7319d369041bb3774da2460dd6da6253d
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-05-19 11:41:47 +0100

    Add new form for adding relationship between food and recipe

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
 
-  resources :recipes, only: [:index, :show, :new, :create, :destroy]
+  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+    resources :recipe_foods, only: %i[new create]
+  end
   resources :inventories, only: %i[index show new create destroy]
 
   get '/public_recipes', to: 'recipes#public_recipes'

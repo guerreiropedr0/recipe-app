f5daf37e4a18d52ff83ead5979ef29ba9a5a359d
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-05-20 15:15:04 +0100

    Add integration spec for recipe index view

--- spec/system/recipes/index_system_spec.rb
+++ spec/system/recipes/index_system_spec.rb
@@ -6,15 +6,13 @@ RSpec.describe 'Recipes', type: :system do
                              password_confirmation: '123456')
   end
 
-  let(:recipe) do
-    FactoryBot.create(:recipe, name: 'French macaroons', preparation_time: '20', cooking_time: '15',
-                               description: 'Delicious snack', public: true,
-                               user_id: user.id)
-  end
-
   context 'index page' do
     before(:each) do
       driven_by(:rack_test)
+
+      @recipe = Recipe.create(name: 'French macaroons', preparation_time: '20', cooking_time: '15',
+                                   description: 'Delicious snack', public: true,
+                                   user_id: user.id)
 
       user.confirm
 
@@ -27,8 +25,40 @@ RSpec.describe 'Recipes', type: :system do
       visit recipes_path
     end
 
-    it 'should show welcoming message' do
+    it 'should display welcoming message' do
       expect(page).to have_content('Your Recipes')
+    end
+
+    it 'should display recipe name' do
+      expect(page).to have_content(@recipe.name)
+    end
+
+    it 'should display recipe description' do
+      expect(page).to have_content(@recipe.description)
+    end
+
+    it 'should find all clickable links' do
+      find_link('REMOVE')
+      find_link(@recipe.name)
+      find_link('New Recipe')
+    end
+
+    it 'should click remove link and remove recipe' do
+      click_link('REMOVE')
+
+      expect(page).to_not have_content(@recipe.name)
+    end
+
+    it 'should click new recipe link and redirect to new recipe form' do
+      click_link('New Recipe')
+
+      expect(page).to have_current_path(new_recipe_path)
+    end
+
+    it 'should click recipe link and redirect to recipe details' do
+      click_link(@recipe.name)
+
+      expect(page).to have_current_path(recipe_path(id: @recipe.id))
     end
   end
 end

c05a2555c28e472635e5ee88f8acc0c83af0c4b3
tYohannes <teklegy@gmail.com>
2022-05-20 23:35:50 +0300

    Add Ivalid Input examples for create new food integration spec

--- spec/system/foods/new_system_spec.rb
+++ spec/system/foods/new_system_spec.rb
@@ -5,22 +5,69 @@ RSpec.describe 'Foods', type: :system do
     driven_by(:rack_test)
   end
 
-  it 'add new food should create new food' do
-    visit '/users/sign_in'
-    within('#new_user') do
-      fill_in 'Email', with: 'adam@domain.com'
-      fill_in 'Password', with: 'password'
+  context 'add new food page' do
+    before :each do
+      visit '/users/sign_in'
+      within('#new_user') do
+        fill_in 'Email', with: 'adam@domain.com'
+        fill_in 'Password', with: 'password'
+      end
+      click_button 'Log in'
     end
-    click_button 'Log in'
-    visit foods_path
 
-    click_link('Add Food')
-    fill_in 'Name', with: 'Orange'
-    fill_in 'Measurement unit', with: 'kg'
-    fill_in 'Price', with: 5.5
-    find('input[name="commit"]').click
+    it "should render add new food view" do
+      visit foods_path
+      click_link('Add Food')
 
-    expect(current_path).to eq foods_path
-    expect(page).to have_content('Orange')
+      expect(page).to have_field("Name")
+      expect(page).to have_field("Measurement unit")
+      expect(page).to have_field("Price")
+      expect(page).to have_selector('input[name="commit"]')
+    end
+
+    it 'should create new food with valid inputs' do
+      visit foods_path
+      click_link('Add Food')
+      fill_in 'Name', with: 'Orange'
+      fill_in 'Measurement unit', with: 'kg'
+      fill_in 'Price', with: 5.5
+      find('input[name="commit"]').click
+
+      expect(current_path).to eq foods_path
+      expect(page).to have_content('Orange')
+    end
+
+    it 'should display error messages on empty name' do
+      visit foods_path
+      click_link('Add Food')
+      fill_in 'Name', with: ''
+      fill_in 'Measurement unit', with: 'kg'
+      fill_in 'Price', with: 5.5
+      find('input[name="commit"]').click
+
+      expect(page).to have_content('Name can\'t be blank')
+    end
+
+    it 'should display error messages on empty measurement unit' do
+      visit foods_path
+      click_link('Add Food')
+      fill_in 'Name', with: 'Orange'
+      fill_in 'Measurement unit', with: ''
+      fill_in 'Price', with: 5.5
+      find('input[name="commit"]').click
+
+      expect(page).to have_content('Measurement unit can\'t be blank')
+    end
+
+    it 'should display error messages on price less than or equal to zero' do
+      visit foods_path
+      click_link('Add Food')
+      fill_in 'Name', with: 'Orange'
+      fill_in 'Measurement unit', with: ''
+      fill_in 'Price', with: 0.0
+      find('input[name="commit"]').click
+
+      expect(page).to have_content('Price must be greater than 0.0')
+    end
   end
 end

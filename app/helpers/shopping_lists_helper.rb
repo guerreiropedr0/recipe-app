3da92cf1cd17011c395a26a23a8e268219988c87
tYohannes <teklegy@gmail.com>
2022-05-21 11:59:40 +0300

    Fix rubocop errors

--- app/helpers/shopping_lists_helper.rb
+++ app/helpers/shopping_lists_helper.rb
@@ -9,22 +9,22 @@ module ShoppingListsHelper
       target_food = food(recipe_food.food_id)
       @inventory_foods.each do |inventory_food|
         quantity_to_shop = recipe_food.quantity - inventory_food.quantity
-        if inventory_food.food_id == recipe_food.food_id && quantity_to_shop > 0
-          is_food_in_inventory = true
-          @shoppinglist.push({ food_id: recipe_food.food_id,
-                              food_name: target_food.name,
-                              food_uom: target_food.measurement_unit,
-                              quantity: quantity_to_shop,
-                              price: target_food.price })
-        end
-      end
-      unless is_food_in_inventory
+        next unless inventory_food.food_id == recipe_food.food_id && quantity_to_shop.positive?
+
+        is_food_in_inventory = true
         @shoppinglist.push({ food_id: recipe_food.food_id,
-                            food_name: target_food.name,
-                            food_uom: target_food.measurement_unit,
-                            quantity: recipe_food.quantity,
-                            price: target_food.price })
+                             food_name: target_food.name,
+                             food_uom: target_food.measurement_unit,
+                             quantity: quantity_to_shop,
+                             price: target_food.price })
       end
+      next if is_food_in_inventory
+
+      @shoppinglist.push({ food_id: recipe_food.food_id,
+                           food_name: target_food.name,
+                           food_uom: target_food.measurement_unit,
+                           quantity: recipe_food.quantity,
+                           price: target_food.price })
     end
 
     @shoppinglist
@@ -35,7 +35,7 @@ module ShoppingListsHelper
     shoping_list.each do |item|
       total_value += item[:quantity] * item[:price]
     end
-    
+
     total_value
   end
 
